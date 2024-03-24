e81a865f781f40084bf2bd6f7e7c964241c7dc7f
MaapTom <marcoantoniopereira41@gmail.com>
2024-03-23 21:00:23 -0300

    rename hellowordl component

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -1,11 +1,11 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import WordleBoard from '../WordleBoard.vue'
 
 describe('HelloWordle', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Wordle' } })
+    const wrapper = mount(WordleBoard, { props: { msg: 'Hello Wordle' } })
     expect(wrapper.text()).toContain('Hello Wordle')
   })
 })
