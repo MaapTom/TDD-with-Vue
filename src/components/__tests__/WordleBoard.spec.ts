efa93dbcebd2cc4d7b93031b96d48713c9059561
MaapTom <marcoantoniopereira41@gmail.com>
2024-03-23 22:01:22 -0300

    Extract arrange phase to beforeEach

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -5,9 +5,12 @@ import { DEFEAT_MESSAGE } from '@/settings'
 
 describe('HelloWordle', () => {
   let wordOfTheDay = "TESTS"
-  test("a victory message appears when the user makes a guess that matches the word of the day", async() => {
-    const wrapper = mount(WordleBoard, { props: { wordOfTheDay } })
+  let wrapper: ReturnType<typeof mount>;
+  beforeEach(() => {
+    wrapper = mount(WordleBoard, { props: { wordOfTheDay } })
+  })
 
+  test("a victory message appears when the user makes a guess that matches the word of the day", async() => {
     const guessInput = wrapper.find("input[type='text']")
     await guessInput.setValue("TESTS")
     await guessInput.trigger("keydown.enter")
@@ -16,8 +19,6 @@ describe('HelloWordle', () => {
   })
 
   test("a defeat message appears if the user makes a guess that is incorrect", async() => {
-    const wrapper = mount(WordleBoard, { props: { wordOfTheDay } })
-
     const guessInput = wrapper.find("input[type='text']")
     await guessInput.setValue("WRONG")
     await guessInput.trigger("keydown.enter")
@@ -26,8 +27,6 @@ describe('HelloWordle', () => {
   })
 
   test("no end-of-game message appears if the user has not yet made a guess", async() => {
-    const wrapper = mount(WordleBoard, { props: { wordOfTheDay } })
-
     expect(wrapper.text()).not.toContain(VICTORY_MESSAGE)
     expect(wrapper.text()).not.toContain(DEFEAT_MESSAGE)
   })
